9fdd4ff2f56cd68b0919fb7243f4cc5ed45c5ea1
Steve Torrens <stevetorrens@Steves-MacBook-Air.local>
2018-06-12 10:32:40 +1200

    read data in from a file

--- ransom-note.js
+++ ransom-note.js
@@ -1,5 +1,29 @@
+//https://www.hackerrank.com/challenges/ctci-ransom-note/problem
 
+const fs = require('fs')
 
+readFile('ransom-note')
+
+//This is an async function
+function readFile(dataFileName) {
+    var filePath = './data/' + dataFileName 
+    fs.readFile(filePath, 'utf8', (err, inputString) => {
+     if (err) throw err
+     console.log({inputString})
+     main(inputString)
+   }); 
+ }
+
+function main(inputString) {
+
+    readLine = inputString.split('\n')
+
+    const magazine = readLine[1].split(' ')
+
+    const note = readLine[2].split(' ')
+    
+    console.log(checkMagazine(magazine, note))
+}
 
 function checkMagazine(magazine, note) {
 //Set result = yes    
@@ -7,7 +31,8 @@ function checkMagazine(magazine, note) {
 //check if noteString is in magazine array
 //if yes filter out noteString from magazine, if result = no
   let result = 'Yes'
-  let cutMagazine = magazine
+  let cutMagazine = [...magazine]
+  console.log(magazine)
   for (let noteString of note) {
     if (cutMagazine.includes(noteString)) {
       cutMagazine.splice(cutMagazine.findIndex((string) => string == noteString),1)  
@@ -20,11 +45,3 @@ function checkMagazine(magazine, note) {
   return result  
 }
 
-function main() {
-    let magazine = ['give', 'me', 'one', 'grand', 'today', 'night']
-    let note = ['give', 'one', 'grand', 'today']
-    console.log(magazine)
-    console.log(checkMagazine(magazine, note))
-}
-
-main()
